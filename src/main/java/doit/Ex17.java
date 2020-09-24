dedf9159bef6ace2dd1e6be7d5971a23465c81cc
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-09-24 14:22:06 +0900

    09. 23

--- src/main/java/doit/Ex17.java
+++ src/main/java/doit/Ex17.java
@@ -4,17 +4,17 @@ public class Ex17 {
 
   static void npira(int n) {
     for (int i = 1; i <= n; i++) {
-      for (int k = i; k <= n-1; k++) {
+      for (int k = i; k <= n - 1; k++) {
         System.out.print(" ");
       }
-      for (int j = 0; j < (i-1)*2+1; j++) {
-        System.out.print(i%10);
+      for (int j = 0; j < (i - 1) * 2 + 1; j++) {
+        System.out.print(i % 10);
       }
       System.out.println();
     }
   }
 
   public static void main(String[] args) {
-npira(6);
+    npira(6);
   }
 }
