791c9dd2e0d8c3535850f81e4e94c8e6c60d7a4f
sungwon <48509269+zhtmr@users.noreply.github.com>
2020-09-29 15:12:36 +0900

    09. 29 제네릭

--- src/main/java/doit/Ex.java
+++ src/main/java/doit/Ex.java
@@ -44,9 +44,16 @@ public class Ex {
       // Box<Toy> 에 들어갈수 있는건 모두 가능하다. (Car, Toy, Robot).
       // 그러나 Box<Toy> 인데 set()에 Plastic 이 오면 자식타입에 부모타입을 담게되므로 컴파일 불가
       box.set(n);
-//      Object object = box.get(); // Object 로 받는건 오류 위험성이 있다.
+//    Object object = box.get();
+//    Object 로 받는건 오류 위험성이 있다. Box<Toy>, Box<Plastic> 둘 중 뭐가 올지 모름.
+//    Toy 가 오면 Plastic, Toy 타입 둘다 되지만, Plastic 이 왔을땐 Toy 타입은 안된다.
+//    컴파일 단계에서 리턴타입을 정할 수 없기때문에 오류
     }
 
+    /*
+    *   결국 Box<?> 에서 ? 의 범위를 두게 되면(<? super Toy>, <? extends Toy>) class Box<T> 의 타입이 결정되지 않는다는 방식으로
+    *   의도적으로 컴파일 오류를 일으켜 코드의 안전성을 높인다.
+    * */
   }
 
   public static void main(String[] args) {
